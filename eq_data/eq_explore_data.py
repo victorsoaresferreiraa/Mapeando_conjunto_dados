70eff05c72e17d029f60ac2571d313d68f42da18
VictorSoaresFerreira <129811759+victorsoaresferreiraa@users.noreply.github.com>
2024-04-05 21:23:55 -0300

    tentando corrigir um erro

--- eq_data/eq_explore_data.py
+++ eq_data/eq_explore_data.py
@@ -4,18 +4,18 @@ import json
 import plotly.express as px 
 
 #le os dados como uma string e os converte em um objeto python
-path = Path('eq_data\eq_data_30_day_m1.geojson')
+path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
 
 #Cria uma versao mais legivel do arquivo de dados 
-path = Path('eq_data\readable_eq_data_.geojson')
+path = Path('eq_data/readable_eq_data_.geojson')
 readable_contents = json.dumps(all_eq_data, indent=4)
 path.write_text(readable_contents)
 
 #Examina todos os terremotos no conjunto de dados 
-all_eq_dicts = all_eq_data['Features']
+all_eq_dicts = all_eq_data['features']
 
 mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
